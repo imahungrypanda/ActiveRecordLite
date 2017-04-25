dda8490c499e6429d9de8447dd1ed41b38393d8d
Steve Olsen <stevelolsen@gmail.com>
2017-04-25 14:17:40 -0700

    Progress on phase 01

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -3,20 +3,33 @@ require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.irregular 'human', 'humans'
+end
+
 class SQLObject
   def self.columns
-    # ...
+    return @columns if instance_variable_defined?("@columns")
+
+    @columns = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        cats
+    SQL
+
+    @columns = @columns.first.map(&:to_sym)
   end
 
   def self.finalize!
   end
 
   def self.table_name=(table_name)
-    # ...
+    define_method("#{table_name.downcase.pluralize}=") { |el| instance_variable_set("@#{table_name}", el) }
   end
 
   def self.table_name
-    # ...
+    name.downcase.pluralize.to_s
   end
 
   def self.all
